73fd18d8c7200e6d6486484c0c3622a551bee267
ricardoervey <ricardo@ervey.nyc>
2024-09-03 08:50:10 -0400

    Add files via upload

---
+++ js/typed-text.js
@@ -0,0 +1,14 @@
+var string = "hello Ion!";
+var array = string.split("");
+var timer;
+
+function frameLooper () {
+	if (array.length > 0) {
+		document.getElementById("text").innerHTML += array.shift();
+	} else {
+		clearTimeout(timer);
+			}
+	loopTimer = setTimeout('frameLooper()',50);
+
+}
+frameLooper();
